41ce4fcd8d2c0de72f9e5be2be72896296ca15f6
jonesramos01 <jonjones.rj@gmail.com>
2021-11-08 09:14:43 -0300

    commit

---
+++ JS/2.CONDICIONAIS/EX1: VELOCIDADE PERMITIDA/script.js
@@ -0,0 +1,11 @@
+var vel = 70 //velocidade
+if(vel <= 60){
+    console.log(`A velocidade do seu carro é ${vel} km/h`);
+    console.log(`Digija sempre usando cinto de segurança!`);
+} else {
+    console.log(`A velocidade do seu carro é ${vel} km/h`);
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`);
+    console.log(`Digija sempre usando cinto de segurança!`);
+}
+
+
---
+++ JS/2.CONDICIONAIS/EX2: DE ONDE VOCÊ É/script.js
@@ -0,0 +1,7 @@
+var país = "CAN"
+console.log(`Seu país é ${país}`);
+if(país == "Brasil"){
+    console.log(`Você é brasileiro!`);
+} else {
+    console.log(`Você é estrangeiro!`);
+}
