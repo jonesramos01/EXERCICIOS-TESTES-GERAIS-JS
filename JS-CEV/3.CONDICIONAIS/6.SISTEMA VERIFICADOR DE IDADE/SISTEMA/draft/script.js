303ed1a2b6ba3108cc7e17ada8c9d1a0cc804e8d
jonesramos01 <jonjones.rj@gmail.com>
2021-11-26 08:53:53 -0300

    commit

--- JS-CEV/3.CONDICIONAIS/6.SISTEMA VERIFICADOR DE IDADE/SISTEMA/draft/script.js
+++ JS-CEV/3.CONDICIONAIS/6.SISTEMA VERIFICADOR DE IDADE/SISTEMA/draft/script.js
@@ -1,12 +1,21 @@
 function verificar(){
+    //DATA ATUAL (AUTOMÁTICO):
     var dataAtual = new Date();
-    var anoAtual = (dataAtual.value)
+    var anoAtual = dataAtual.getFullYear();
 
+    //DATA DO FORMULÁRIO:
     var anoForm = window.document.getElementById("txtano");
 
-    var idade = ""
-
+    //RESULTADO:
     var res = window.document.getElementById("res");
+    
+    //IMAGEM:
 
-    if()
+
+    if(idade == 0 || idade == ""){
+        //alert(`[ERRO] Por favor, entre com os dados corretamente!`);
+
+    } else if(idade ){
+
+    }
 }
